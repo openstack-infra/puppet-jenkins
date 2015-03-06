acc7abdbe014f63e27d685faefe16c8da173e5f2
Yolanda Robla <yolanda.robla-mota@hp.com>
2015-03-06 14:38:28 +0100

    Add extension settings in jjb
    Enable passing extra settings to jjb,
    so settings needed for extra modules can be
    easily set on jenkins_jobs.ini
    
    Extensions must be sent with the format
    $extensions = [
      {
        'name'       => 'extension',
        'parameters' => [
          {
            'name'  => 'parameter_name',
            'value' => 'parameter_value',
          }
        ]
      }
    ]
    Enable passing hipchat_authtoken parameter to
    enable HipChat integration on jjb.
    
    Change-Id: I7317eda0ad245e3320577d194e545eb4edaf3fac

--- manifests/job_builder.pp
+++ manifests/job_builder.pp
@@ -8,6 +8,7 @@ class jenkins::job_builder (
   $git_url = 'https://git.openstack.org/openstack-infra/jenkins-job-builder',
   $config_dir = '',
   $jenkins_jobs_update_timeout = '600',
+  $extensions = [],
 ) {
 
   # A lot of things need yaml, be conservative requiring this package to avoid
