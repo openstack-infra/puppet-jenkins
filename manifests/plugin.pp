f3585ba7dc3b6f6dbbbb1b7ff8c37166fe845d29
Ian Wienand <iwienand@redhat.com>
2015-07-17 15:41:14 +1000

    Gate on puppet-lint-version_comparison-check
    Use versioncmp, otherwise with puppet 4 (as on Fedora 22) you get "A
    String is not comparable to a non String"
    
    Change-Id: Ic8b0fd2a19cbb90e1d57e3025a3d2513ebada2e2
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -18,18 +18,17 @@
 #
 
 define jenkins::plugin(
-  $version=0,
+  $version='latest',
   $pin=false,
 ) {
   $plugin            = "${name}.hpi"
   $plugin_dir        = '/var/lib/jenkins/plugins'
   $plugin_parent_dir = '/var/lib/jenkins'
 
-  if ($version != 0) {
+  if ($version == 'latest') {
+    $base_url = 'http://updates.jenkins-ci.org/latest'
+  } else {
     $base_url = "http://updates.jenkins-ci.org/download/plugins/${name}/${version}"
-  }
-  else {
-    $base_url   = 'http://updates.jenkins-ci.org/latest'
   }
 
   if (!defined(File[$plugin_dir])) {
