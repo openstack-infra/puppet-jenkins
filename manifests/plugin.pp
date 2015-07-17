325b0fa5871645da2c0993aa8bf6e165383bbfd6
Paul Belanger <pabelanger@redhat.com>
2015-07-17 12:56:49 -0400

    Add Gemfile and puppet 4 checks
    In anticipation of puppet 4, start trying to deal with puppet 4 things
    that can be helpfully predicted by puppet lint plugins.
    
    Change-Id: I7474560293012479e80e944ae6d38e2836174298
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -58,15 +58,15 @@ define jenkins::plugin(
   }
 
   exec { "download-${name}" :
-    command  => "wget --no-check-certificate ${base_url}/${plugin}",
-    cwd      => $plugin_dir,
-    require  => File[$plugin_dir],
-    path     => ['/usr/bin', '/usr/sbin',],
-    user     => 'jenkins',
-    unless   => "test -f ${plugin_dir}/${name}.?pi",
+    command => "wget --no-check-certificate ${base_url}/${plugin}",
+    cwd     => $plugin_dir,
+    require => File[$plugin_dir],
+    path    => ['/usr/bin', '/usr/sbin',],
+    user    => 'jenkins',
+    unless  => "test -f ${plugin_dir}/${name}.?pi",
 #    OpenStack modification: don't auto-restart jenkins so we can control
 #    outage timing better.
-#    notify   => Service['jenkins'],
+#    notify  => Service['jenkins'],
   }
 
   if ($pin) {
