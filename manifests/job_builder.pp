3651901d10e7fe464e9799b38c9fa82c1a228148
Paul Belanger <pabelanger@redhat.com>
2015-08-05 13:16:35 -0400

    Require ssh_key, username, password and url
    Default these values to '' does provide any real value. We should
    force the user to setup something other then an empty string.
    
    Change-Id: I04da6afae5d39a20f9601b0dbd834e97d280d028
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- manifests/job_builder.pp
+++ manifests/job_builder.pp
@@ -1,9 +1,9 @@
 # == Class: jenkins::job_builder
 #
 class jenkins::job_builder (
-  $url = '',
-  $username = '',
-  $password = '',
+  $url,
+  $username,
+  $password,
   $git_revision = 'master',
   $git_url = 'https://git.openstack.org/openstack-infra/jenkins-job-builder',
   $config_dir = '',
