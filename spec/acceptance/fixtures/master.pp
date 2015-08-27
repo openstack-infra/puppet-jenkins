9b4958abc0d88eb4a4da5baa316f32293a78474f
Glauco Oliveira <gvinici@thoughtworks.com>
2015-08-27 14:05:46 -0300

    Add acceptance tests for puppet-jenkins.
    Add acceptance tests for puppet-jenkins module so that once the module
    is applied we check if files were created, packages were installed and
    services were started.
    
    Change-Id: Ib69055af29de08902aba8a9e1b12d716584a2e3d
    Co-Authored-By: Bruno Tavares <btavare@thoughtworks.com>
    Co-Authored-By: Danilo Ramalho <dramalho@thoughtworks.com>

---
+++ spec/acceptance/fixtures/master.pp
@@ -0,0 +1,19 @@
+class { '::jenkins::jenkinsuser':
+  ssh_key => 'sshkey',
+}
+
+class { '::jenkins::master':
+  vhost_name              => 'localhost',
+  jenkins_ssh_private_key => file('/tmp/jenkins-ssh-keys/ssh_rsa_key'),
+  jenkins_ssh_public_key  => file('/tmp/jenkins-ssh-keys/ssh_rsa_key.pub'),
+  require                 => Class['::jenkins::jenkinsuser'],
+}
+
+class { '::jenkins::job_builder':
+  url                         => 'https://localhost',
+  username                    => 'jenkins',
+  password                    => 'secret',
+  jenkins_jobs_update_timeout => 1200,
+  config_dir                  => '/etc/project-config/jenkins',
+  require                     => Class['::jenkins::master'],
+}
