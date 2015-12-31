943efb9ae3c90d7b407f4aba1dd51d469cca3433
Isaac Beckman <isaacb@il.ibm.com>
2015-12-31 15:07:40 +0200

    Customize jenkins.default by template
    Turn jenkins.default to a template. This enables to customize it
    easily. This is useful for downstream projects where it is needed
    to tune jenkins.default parameters. Either Pass the required
    parameter to jenkins class or pass a customized jenkins.default
    template in jenkins_default
    For backward compatibility, default values are set in the class
    parameters list.
    
    Change-Id: Ice0e4ffa2fffd041d8bcc4a0b323ffae7ba2b0c8

--- manifests/master.pp
+++ manifests/master.pp
@@ -1,5 +1,12 @@
 # == Class: jenkins::master
 #
+# This class will install and configure Jenkins master
+#
+# Setting configuration parameters is done by either setting
+# $jenkins_default to point to a customized file/template or by
+# setting the specific parameters in the class
+#
+
 class jenkins::master(
   $logo = '',
   $vhost_name = $::fqdn,
@@ -12,7 +19,11 @@ class jenkins::master(
   $ssl_chain_file_contents = '', # If left empty puppet will not create file.
   $jenkins_ssh_private_key = '',
   $jenkins_ssh_public_key = '',
-  $jenkins_default = 'puppet:///modules/jenkins/jenkins.default',
+  $jenkins_default = undef, # Can be a custom template
+  $java_args = '"-Xloggc:/var/log/jenkins/gc.log -XX:+PrintGCDetails -Xmx12g -Dorg.kohsuke.stapler.compression.CompressionFilter.disabled=true -Djava.util.logging.config.file=/var/lib/jenkins/logger.conf"',
+  $run_standalone = true,
+  $max_open_files = 8192,
+  $http_port = 8080,
 ) {
   include ::pip
   include ::apt
@@ -123,12 +134,30 @@ class jenkins::master(
     command     => 'apt-get update',
   }
 
-  file { '/etc/default/jenkins':
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => $jenkins_default,
+  # Template uses:
+  # - $java_args
+  # - $run_standalone
+  # - $max_open_files
+  # - $http_port
+
+  if ! $jenkins_default {
+    $jenkins_template = 'jenkins/jenkins.default.erb'
+    file { '/etc/default/jenkins':
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template($jenkins_template),
+    }
+  }
+  else {
+    file { '/etc/default/jenkins':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      source => $jenkins_default
+    }
   }
 
   file { '/var/lib/jenkins':
