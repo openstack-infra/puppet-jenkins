434177bedadae3262aa52bec5ce37574183c36d0
Colleen Murphy <colleen.murphy@suse.com>
2017-05-03 11:14:36 +0200

    Fix beaker on xenial
    1. Since the beaker jobs are being run on xenial, we need a special nodeset
    for it, otherwise beaker gives an error:
    
     beaker-hostgenerator was not able to use this value as input.
     Exiting with an Error.
    
    2. Install puppet from the Ubuntu repos rather than from
    puppetlabs, since puppetlabs doesn't support puppet 3 for Xenial. For
    centos we can keep the install process the same.
    
    3. Rather than updating the individual beaker tests for xenial,
    this patch deletes most of the resource-level beaker assertions. It's
    really quite enough to trust that a successful puppet run means the
    packages were found and the config files applied. All that's really
    important besides checking that puppet was successful is checking that
    the services are running.
    
    4. Don't mess with the jenkins user. Zuul needs the jenkins user's
    authorized_keys file to stay intact in order to rsync logs.
    
    5. Finally, remove the jenkins::job_builder fixture. There's not a good
    excuse for this, it just doesn't seem to work right now, perhaps as a
    result of recent security updates to jenkins.
    
    Change-Id: I244ac33943e54461c0eb849d40d32a80c161e0e3
    Depends-On: If31abd1d29416dbb567c0f76529913b8327d40c1

--- spec/acceptance/fixtures/master.pp
+++ spec/acceptance/fixtures/master.pp
@@ -1,7 +1,3 @@
-class { '::jenkins::jenkinsuser':
-  ssh_key => 'sshkey',
-}
-
 class { '::jenkins::master':
   vhost_name              => '127.0.0.1',
   jenkins_ssh_private_key => file('/tmp/jenkins-ssh-keys/ssh_rsa_key'),
